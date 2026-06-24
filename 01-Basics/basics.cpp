70756f437260a4de1e28c6a67d3b84e904d5c00f
Dibyo Sarkar <55356403+DibyoSarkar@users.noreply.github.com>
2026-06-25 00:46:20 +0600

    Comment out age and mark validation code
    Commented out age and mark validation sections for future reference.

--- 01-Basics/basics.cpp
+++ 01-Basics/basics.cpp
@@ -39,18 +39,47 @@ int main(){
     
     
     // If Else //
-    int age;
-    cin >> age;
-    if(age<0 || age>150){
-        cout << "Are you even a human?";
-    }
-    else{
-        if(age<18){
-            cout << "You're not an Adult";
+    /*
+        int age;
+        cin >> age;
+        if(age<0 || age>150){
+            cout << "Are you even a human?";
         }
         else{
-            cout << "You're an Adult";
+            if(age<18){
+                cout << "You're not an Adult";
+            }
+            else{
+                cout << "You're an Adult";
+            }
         }
-    }
+    */
+    /*
+        int mark;
+        cin >> mark;
+        if(mark>100 || mark<0){
+            cout << "Invalid Mark";
+        }
+        else{
+            if(mark<=25){
+                cout << "You've got F";
+            }
+            else if(mark<=44){
+                cout << "You've got E";
+            }
+            else if(mark<=49){
+                cout << "You've got D";
+            }
+            else if(mark<=59){
+                cout << "You've got C";
+            }
+            else if(mark<=79){
+                cout << "You've got B";
+            }
+            else{
+                cout << "You've got A+";
+            }
+        }
+    */
     
 }
