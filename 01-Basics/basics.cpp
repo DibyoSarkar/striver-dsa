4f2ccac5e7fdfcaa8756ff8e503183cde4590777
DibyoSarkar <55356403+DibyoSarkar@users.noreply.github.com>
2026-06-26 17:06:04 +0600

    Added 1D array example in basics.cpp
    Coded examples for 1D array usage.

--- 01-Basics/basics.cpp
+++ 01-Basics/basics.cpp
@@ -142,5 +142,16 @@ int main(){
 
         cout << ", this will be printed, cause this is outside the conditional statement";
     */
+
+
+    // 1D Array //
+    /*
+        int arr[5];
+        cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+        
+        cout << arr[3] << endl;
+        arr[3] += 10;
+        cout << arr[3];
+    */
     
 }
