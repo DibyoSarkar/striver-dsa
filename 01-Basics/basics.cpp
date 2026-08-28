1d12d8d8ac50f37a1c33a3b6e7dfa0d5464f9efa
DibyoSarkar <55356403+DibyoSarkar@users.noreply.github.com>
2026-08-28 20:58:48 +0530

    Added array and loop combined examples in basics.cpp
    Added array and loop combined usecase in basics.cpp

--- 01-Basics/basics.cpp
+++ 01-Basics/basics.cpp
@@ -308,4 +308,13 @@ int main(){
     /*int num = 10;
     doSomething(num);
     cout << num << endl;*/
+
+    // array and loop combined usecase //
+    /*int arr[5];
+    for(int i=0; i<5; i++){
+        cin >> arr[i];
+    }
+    for(int i=0; i<5; i++){
+        cout << arr[i] << " ";
+    }*/
 }
