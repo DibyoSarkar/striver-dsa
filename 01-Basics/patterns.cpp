46e5319b9623f9508362452c2975b18769b00d35
DibyoSarkar <55356403+DibyoSarkar@users.noreply.github.com>
2026-09-06 21:58:44 +0530

    Included Pattern1 & Pattern2 in patterns.cpp
    Added pattern functions and modified main to use pattern.

--- 01-Basics/patterns.cpp
+++ 01-Basics/patterns.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-
-    // Pattern 1 //
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
+/*
+void pattern1(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
             cout << "* ";
         }
         cout << endl;
     }
+}
+*/
 
+/*
+void pattern2(int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<=i; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+*/
+
+int main(){
+    int t;
+    cin >> t;
+    for(int i=0; i<t; i++){
+        int n;
+        cin >> n;
+        pattern2(n);
+    }
+    
 }
