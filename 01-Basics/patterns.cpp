0c4b5ea70074514c44c65435059787d5245bfbf5
DibyoSarkar <55356403+DibyoSarkar@users.noreply.github.com>
2026-09-06 22:20:35 +0530

    Implemented Pattern 3,4&5 in patterns.cpp
    Added multiple pattern functions to generate different patterns using asterisks and numbers.

--- 01-Basics/patterns.cpp
+++ 01-Basics/patterns.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-/*
+
+
 void pattern1(int n){
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
@@ -10,9 +11,7 @@ void pattern1(int n){
         cout << endl;
     }
 }
-*/
 
-/*
 void pattern2(int n){
     for(int i=0; i<n; i++){
         for(int j=0; j<=i; j++){
@@ -21,7 +20,44 @@ void pattern2(int n){
         cout << endl;
     }
 }
-*/
+
+void pattern3(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern4(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern5(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<n-i+1; j++){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern5alt(int n){
+    for(int i=0; i<n; i++){
+        for(int j=n; j>i; j--){
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+
 
 int main(){
     int t;
