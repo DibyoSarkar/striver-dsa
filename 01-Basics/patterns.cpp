1a9155dc025b33df54be0e904e31d46616c98d0b
Bojack Horseman <55356403+DibyoSarkar@users.noreply.github.com>
2026-09-07 10:58:38 +0530

    Implement Pattern 6,7&8 in patterns.cpp
    Added functions to print various patterns using loops.

--- 01-Basics/patterns.cpp
+++ 01-Basics/patterns.cpp
@@ -57,6 +57,57 @@ void pattern5alt(int n){
     }
 }
 
+void pattern6(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void pattern7(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        //star
+        for(int j=0; j<i*2+1; j++){
+            cout << "*";
+        }
+
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+void pattern8(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+
+        //star
+        for(int j=0; j<n*2 - i*2 - 1; j++){
+            cout << "*";
+        }
+
+        //space
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
 
 
 int main(){
