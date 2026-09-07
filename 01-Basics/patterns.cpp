f799c3a609c2d2e0b8e3b7255da39897d1b2c1bc
Bojack Horseman <55356403+DibyoSarkar@users.noreply.github.com>
2026-09-07 22:31:21 +0530

    Implemented Pattern10 for diamond star pattern in patterns.cpp
    Add pattern10 function to print a diamond pattern of stars.

--- 01-Basics/patterns.cpp
+++ 01-Basics/patterns.cpp
@@ -147,6 +147,17 @@ void pattern9(int n){
     }
 }
 
+void pattern10(int n){
+    for(int i=1; i<=2*n-1; i++){
+        int stars = i;
+        if(stars > n) stars = 2*n-i;
+        for(int j=1; j<=stars; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
 
 
 int main(){
