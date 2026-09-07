ca186bee6497e572886b54b48e825f221b0e72d0
Bojack Horseman <55356403+DibyoSarkar@users.noreply.github.com>
2026-09-07 23:45:18 +0530

    Added Pattern11 and 3versions of Pattern12 in patterns.cpp
    Implemented and explained Pattern11 and 3 different versions of Pattern12 in patterns.cpp

--- 01-Basics/patterns.cpp
+++ 01-Basics/patterns.cpp
@@ -158,6 +158,83 @@ void pattern10(int n){
     }
 }
 
+void pattern11(int n){
+    int start=1;
+    for(int i=0; i<n; i++){
+        if(i%2==0) start = 1;
+        for(int j=0; j<=i; j++) {
+            cout << start << " ";
+            start = 1- start;
+        }
+        cout << endl;
+
+    }
+}
+
+void pattern12(int n){
+    int space = 2*(n-1);
+    for(int i=1; i<=n; i++){
+        //numbers
+        for(int j=1; j<=i; j++){
+            cout << j;
+        }
+
+        //space
+        for(int j=1; j<=space; j++){
+            cout << " ";
+        }
+
+        //numbers
+        for(int j=i; j>=1; j--){
+            cout << j;
+        }
+
+        cout << endl;
+        space -= 2;
+    }
+}
+
+void pattern12alt(int n){
+    for(int i=0; i<n; i++){
+        //numbers
+        for(int j=0; j<=i; j++){
+            cout << j+1;
+        }
+
+        //space
+        for(int j=0; j<(2*n)-(2*i)-2; j++){
+            cout << "-";
+        }
+
+        //numbers
+        for(int j=i; j>=0; j--){
+            cout << j+1;
+        }
+
+        cout << endl;
+    }
+}
+
+void pattern12alt1(int n){
+    for(int i=1; i<=n; i++){
+        //numbers
+        for(int j=1; j<=i; j++){
+            cout << j;
+        }
+
+        //space
+        for(int j=0; j<(2*n)-(2*i); j++){
+            cout << "-";
+        }
+
+        //numbers
+        for(int j=i; j>0; j--){
+            cout << j;
+        }
+
+        cout << endl;
+    }
+}
 
 
 int main(){
