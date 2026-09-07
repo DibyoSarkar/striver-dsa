b732be17b749a6e9823a6e3a297679f9da82ee31
Bojack Horseman <55356403+DibyoSarkar@users.noreply.github.com>
2026-09-07 21:46:34 +0530

    Implemented Pattern9 for star pattern in patterns.cpp
    Added pattern9 function to print a specific star pattern with spaces.

--- 01-Basics/patterns.cpp
+++ 01-Basics/patterns.cpp
@@ -108,6 +108,45 @@ void pattern8(int n){
     }
 }
 
+void pattern9(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        //star
+        for(int j=0; j<i*2+1; j++){
+            cout << "*";
+        }
+
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+
+        //star
+        for(int j=0; j<n*2 - i*2 - 1; j++){
+            cout << "*";
+        }
+
+        //space
+        for(int j=0; j<i; j++){
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
 
 
 int main(){
