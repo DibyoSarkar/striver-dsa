26b7dc785c7506f0b03b7f16c3bd874c45250e10
Bojack Horseman <55356403+DibyoSarkar@users.noreply.github.com>
2026-09-11 01:23:04 +0530

    Added Pattern15 to print letter pattern
    Added a new function 'pattern15' to print a pattern of letters.

--- 01-Basics/patterns.cpp
+++ 01-Basics/patterns.cpp
@@ -256,6 +256,15 @@ void pattern14(int n){
     }
 }
 
+void pattern15(int n){
+    for(int i=0; i<n; i++){
+        for(char ch = 'A'; ch<='A'+(n-i-1); ch++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     int t;
     cin >> t;
