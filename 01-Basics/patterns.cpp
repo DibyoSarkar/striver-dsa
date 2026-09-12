07dee9069a0b31fac34ee4fbd0738b6ea6684d76
Bojack Horseman <55356403+DibyoSarkar@users.noreply.github.com>
2026-09-12 22:56:31 +0530

    Implement pattern17 function for character pattern printing
    Added a new function pattern17 to print a specific character pattern based on input size.

--- 01-Basics/patterns.cpp
+++ 01-Basics/patterns.cpp
@@ -275,6 +275,31 @@ void pattern16(int n){
     }
 }
 
+void pattern17(int n){
+    for(int i=0; i<n; i++){
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        //characters
+        char ch = 'A';
+        int breakpoint = (i*2+1) / 2;
+        for(int j=0; j<i*2+1; j++){
+            cout << ch;
+            if(j < breakpoint) ch++;
+            else ch--;
+        }
+
+        //space
+        for(int j=0; j<n-i-1; j++){
+            cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
 int main(){
     int t;
     cin >> t;
